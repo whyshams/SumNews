68922e7e7d06e726aeb8de9c2da6611a4f40297c
Nuren Shams Chowdhury <chowdhury.noren@gmail.com>
2022-08-29 02:29:55 +0600

    allahbhorsha

--- pages/BDnews/Categorical/[CategoryBd].js
+++ pages/BDnews/Categorical/[CategoryBd].js
@@ -58,8 +58,7 @@ const paginate = pageNumber => setCurrentPage(pageNumber);
       <meta name="description" content="Get category wise News Of Bangladesh from the reputed English Newspaper of the country and summarize them in seconds with our summarizer tool..."/>
       <meta name="keywords" content="Bangladesh news, Bangla News,BD News,NewsBd,bangladesh english news, bangladesh english newspaper,english newspaper bangladesh,summary,summarize news,all bangladesh english news"/>
       <meta name="author" content="Nuren Shams Chowdhury"/>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5159189580385319"
-     crossorigin="anonymous"></script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5159189580385319"></script>
       </Head>
       <div className='selectSticky'>
       <div className='row '>

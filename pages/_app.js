fa7fb90bcebb102539d0a22508d38563505a69a4
Nuren Shams Chowdhury <chowdhury.noren@gmail.com>
2022-08-29 03:28:39 +0600

    anal

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,29 @@
 import  ResultContextProvider  from './Contexts/ResultContextProvider';
 
 import Layout from '../Components/Layout';
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }) {
   return(
       <Layout>
     <ResultContextProvider>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-2DFMRGF7PB" />
+    <Script strategy="afterInteractive" id="google-analytics" >
+      {
+        `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js',new Date());
+        gtag('config','G-2DFMRGF7PB')
+        `
+      }
+    </Script>
 
         <Component {...pageProps} />
     </ResultContextProvider>
-
-      </Layout>
+  </Layout>
      
   
 
